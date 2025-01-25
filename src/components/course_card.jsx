81f57957d823214f9978c4cb419666cde11d0df6
f0rbit <dev@forbit.dev>
2025-01-25 15:58:53 +1030

    fixes

--- src/components/course_card.jsx
+++ src/components/course_card.jsx
@@ -1,5 +1,6 @@
 import { useState, useContext } from "react";
 import { AppData } from "../App";
+import { Pencil } from "./sidebar";
 
 export default function CourseCard({ course }) {
   const { notes, setPage, setSelectedCourse, setSelectedNote, deleteCourse, updateCourseTitle } = useContext(AppData);
@@ -83,9 +84,9 @@ export default function CourseCard({ course }) {
             </a>
           </h1>
         )}
-        <button className="edit-button" onClick={handleEditToggle}>
-          {isEditing ? 'Save' : 'Edit'}
-        </button>
+		<a role="button" onClick={handleEditToggle} style={{display: isEditing ? 'none' : 'block'}}>
+			<Pencil />
+		</a>
       </div>
       <div className="course-notes">
         {recent_3.map((note) => (
@@ -94,7 +95,14 @@ export default function CourseCard({ course }) {
           </a>
         ))}
       </div>
-	  <button onClick={handleDelete} className="delete-button">Delete</button> 
+	  <a role="button" onClick={handleDelete} className="delete-button">
+		<Trash />
+		</a> 
     </div>
   );
+}
+
+
+function Trash() {
+	return <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-trash-2"><path d="M3 6h18"/><path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"/><path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"/><line x1="10" x2="10" y1="11" y2="17"/><line x1="14" x2="14" y1="11" y2="17"/></svg>
 }
